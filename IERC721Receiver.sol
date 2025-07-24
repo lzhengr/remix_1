034fdf2e2f85c03c038e18fe6ce4ba14c9cd4c41
lzhengr <397278392@qq.com>
2025-07-24 09:05:03 +0800

    solidity 103 -2

---
+++ IERC721Receiver.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+// ERC721 receiver interface: Contracts must implement this interface to receive ERC721 tokens via safe transfers.
+interface IERC721Receiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
